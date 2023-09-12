5a53e667f2aab37fc3fec452c0827653d124931a
GermanS04 <german.salasmar@gmail.com>
2023-09-12 02:11:50 -0600

    fix(Navbar container)

---
+++ Artemis/src/pages/Ingresos/Ingresos.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Ingresos = () => {
+
+    return (
+        <h1>
+            Ingresos
+        </h1>
+    )
+}
+
+export default Ingresos
---
+++ Artemis/src/pages/Reporte/Reporte.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Reporte = () => {
+
+    return (
+        <h1>
+            Reporte
+        </h1>
+    )
+}
+
+export default Reporte
