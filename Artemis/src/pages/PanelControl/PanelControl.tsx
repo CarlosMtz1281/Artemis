3b297397d8d692e38a68bba9070b00602326c7ed
GermanS04 <german.salasmar@gmail.com>
2023-09-15 11:54:34 -0600

    start(widget)

--- Artemis/src/pages/PanelControl/PanelControl.tsx
+++ Artemis/src/pages/PanelControl/PanelControl.tsx
@@ -1,5 +1,26 @@
 import React from 'react'
 
+// Material UI
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
+
+import {colors} from '../../css/constants'
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#D3D3D3',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: colors.whiteTxt,
+  minWidth: "56vw",
+  minHeight: "40vh",
+}));
+
 // STYLE
 import {styles} from './styles'
 import './PanelControl.css'
@@ -7,7 +28,18 @@ import './PanelControl.css'
 const PanelControl = () => {
     return (
         <>
-        <h1 style={styles.mainTitle}>Panel de Control </h1>
+        <Box> 
+            <Grid>
+                <Item>
+                    wkjenfwkjefnekwjf
+                </Item>
+            </Grid>
+            <Grid>
+                <Item>
+                    ernekrfjnerkjn
+                </Item>
+            </Grid>
+        </Box>
         </>
     )
 }
